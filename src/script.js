573ee298b3a769e229bbca0c7b3c4b89563ee352
Pablo Melo Franca FIlho <pablomelofrancafilho11@gmail.com>
2026-07-31 11:16:41 -0300

    add new button features

--- src/script.js
+++ src/script.js
@@ -1,8 +1,13 @@
-var firstFactor = document.getElementById("factor-1");
-var numbers = document.querySelectorAll(".btn-number");
-numbers.forEach(function (element, number) {
-    number = Number(element.textContent);
-    element.addEventListener("click", function () {
-        firstFactor.textContent = 0 ? firstFactor.textContent = number : firstFactor.textContent += number;
+"use strict";
+const firstFactor = document.getElementById("factor-1");
+const numbers = document.querySelectorAll(".btn-number");
+const btnClear = document.getElementById("clear");
+numbers.forEach(function (element) {
+    const number = element.textContent ?? "";
+    element.addEventListener("click", () => {
+        firstFactor.textContent === "0" ? firstFactor.textContent = number : firstFactor.textContent += number;
     });
+});
+btnClear.addEventListener("click", () => {
+    firstFactor.textContent = "0";
 });
--- src/script.ts
+++ src/script.ts
@@ -1,11 +1,17 @@
-const firstFactor = document.getElementById("factor-1") as HTMLElement;
+const firstFactor = document.getElementById("factor-1") as HTMLParagraphElement;
 const numbers = document.querySelectorAll(".btn-number") as NodeListOf<HTMLElement>;
+const btnClear = document.getElementById("clear") as HTMLElement;
 
 
-numbers.forEach(function(element, number) {
-    number = Number(element.textContent);
+numbers.forEach(function(element) {
+    const number = element.textContent ?? "";
     element.addEventListener("click", () => {
-        firstFactor.textContent = 0 ? firstFactor.textContent = number : firstFactor.textContent += number;
-    })
+        firstFactor.textContent === "0" ? firstFactor.textContent = number : firstFactor.textContent += number;
+    });
+});
+
+btnClear.addEventListener("click", () => {
+    firstFactor.textContent = "0";
 })
+
 
