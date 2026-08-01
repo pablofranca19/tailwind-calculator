c062814b02f3ee3715a23284b5445f0dbdebf3e5
Pablo Melo Franca FIlho <pablomelofrancafilho11@gmail.com>
2026-08-01 10:29:45 -0300

    .ts file mirror

--- src/script.js
+++ src/script.js
@@ -2,10 +2,19 @@
 const firstFactor = document.getElementById("factor-1");
 const numbers = document.querySelectorAll(".btn-number");
 const btnClear = document.getElementById("clear");
+const operators = document.querySelectorAll(".operator");
+const equalOperator = document.getElementById("equals");
 numbers.forEach(function (element) {
     const number = element.textContent ?? "";
     element.addEventListener("click", () => {
         firstFactor.textContent === "0" ? firstFactor.textContent = number : firstFactor.textContent += number;
+        operators.forEach(function (operator) {
+            operator.addEventListener("click", () => {
+                const parseFirstFactor = parseInt(firstFactor.textContent);
+                const selectedOperator = operator.textContent;
+                firstFactor.textContent = selectedOperator;
+            });
+        });
     });
 });
 btnClear.addEventListener("click", () => {
