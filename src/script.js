50fe20bf236345ae7937f00bd4e580f12b290367
Pablo Melo Franca FIlho <pablomelofrancafilho11@gmail.com>
2026-08-03 16:33:11 -0300

    add clear button logic

--- src/script.js
+++ src/script.js
@@ -4,19 +4,26 @@ const numbers = document.querySelectorAll(".btn-number");
 const btnClear = document.getElementById("clear");
 const operators = document.querySelectorAll(".operator");
 const equalOperator = document.getElementById("equals");
-numbers.forEach(function (element) {
-    const number = element.textContent ?? "";
-    element.addEventListener("click", () => {
-        firstFactor.textContent === "0" ? firstFactor.textContent = number : firstFactor.textContent += number;
-        operators.forEach(function (operator) {
-            operator.addEventListener("click", () => {
-                const parseFirstFactor = parseInt(firstFactor.textContent);
-                const selectedOperator = operator.textContent;
-                firstFactor.textContent = selectedOperator;
-            });
-        });
-    });
+let factor1 = null;
+let operator;
+let factor2 = null;
+let resetFlag;
+numbers.forEach(function (number) {
 });
 btnClear.addEventListener("click", () => {
     firstFactor.textContent = "0";
+    operator == null;
+    factor1 == null;
+    factor2 == null;
+    resetFlag = true;
 });
+function calculate(element1, operator, element2) {
+    switch (operator) {
+        case "+": return element1 + element2;
+        case "-": return element1 - element2;
+        case "X": return element1 * element2;
+        case "/": return element1 / element2;
+        default:
+            return element2;
+    }
+}
--- src/script.ts
+++ src/script.ts
@@ -7,27 +7,21 @@ const equalOperator = document.getElementById("equals");
 let factor1: string | null = null;
 let operator: string;
 let factor2: string | null = null;
+let resetFlag: boolean;
 
-numbers.forEach(function(element) {
-    const number = element.textContent ?? "";
-    element.addEventListener("click", () => {
-        firstFactor.textContent === "0" ? firstFactor.textContent = number : firstFactor.textContent += number;
-        operators.forEach(function(operator) {
-            operator.addEventListener("click", () => {
-                const parseFirstFactor = parseInt(firstFactor.textContent);
-                const selectedOperator = operator.textContent;
-                firstFactor.textContent = selectedOperator;
-                
-            });
-        });
-    });
+numbers.forEach(function(number) {
+    
 });
 
 btnClear.addEventListener("click", () => {
     firstFactor.textContent = "0";
+    operator == null;
+    factor1 == null;
+    factor2 == null;
+    resetFlag = true;
 });
 
-function calculate(element1: number, operator: string, element2: number) {
+function calculate(element1: number, operator: string, element2: number): number {
     switch (operator) {
         case "+": return element1 + element2;
         case "-": return element1 - element2;
