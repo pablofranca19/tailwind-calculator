9220052b360db71703aafe6e908a273ab0406b13
Pablo Melo Franca FIlho <pablomelofrancafilho11@gmail.com>
2026-08-04 11:38:11 -0300

    testing calculation logic

--- src/script.ts
+++ src/script.ts
@@ -2,23 +2,46 @@ const firstFactor = document.getElementById("factor-1") as HTMLParagraphElement;
 const numbers = document.querySelectorAll(".btn-number") as NodeListOf<HTMLElement>;
 const btnClear = document.getElementById("clear") as HTMLElement;
 const operators = document.querySelectorAll(".operator") as NodeListOf<HTMLElement>;
-const equalOperator = document.getElementById("equals");
+const equalOperator = document.getElementById("equals") as HTMLElement;
 
 let factor1: string | null = null;
-let operator: string;
+let operatorStr: string;
 let factor2: string | null = null;
-let resetFlag: boolean;
+let pressFlag: boolean;
+let isOperatorPressed: boolean;
 
 numbers.forEach(function(number) {
-    
+    number.addEventListener("click", () => {
+        firstFactor.textContent === "0" || (pressFlag == true && firstFactor.textContent == operatorStr) ? firstFactor.textContent = number.textContent : firstFactor.textContent += number.textContent;    
+    });
 });
+
+operators.forEach(function(operator) {
+    operator.addEventListener("click", () => {
+        factor1 = firstFactor.textContent;
+        firstFactor.textContent = operator.textContent;
+        operatorStr = firstFactor.textContent;
+        pressFlag = true;
+        isOperatorPressed = true;
+    });
+});
+
 
 btnClear.addEventListener("click", () => {
     firstFactor.textContent = "0";
-    operator == null;
-    factor1 == null;
-    factor2 == null;
-    resetFlag = true;
+    operatorStr = "";
+    factor1 = null;
+    factor2 = null;
+    pressFlag = false;
+    isOperatorPressed = false;
+});
+
+factor2 = "10";
+
+equalOperator.addEventListener("click", () => {
+    console.log(factor1, operatorStr, factor2);
+    console.log(calculate(Number(factor1), operatorStr, Number(factor2)));
+    firstFactor.textContent = String(calculate(Number(factor1), operatorStr, Number(factor2)));
 });
 
 function calculate(element1: number, operator: string, element2: number): number {
@@ -31,4 +54,3 @@ function calculate(element1: number, operator: string, element2: number): number
             return element2;
     }
 }
-
